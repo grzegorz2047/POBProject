bfb9222a4242433a3be9e30f48732f6c32b61cff
Grzegorz2047 <grzegorz2047@gmail.com>
2016-06-13 19:20:51 +0200

    changing concept of project.

--- src/main/java/pl/grzegorz2047/pobproject/Main.java
+++ src/main/java/pl/grzegorz2047/pobproject/Main.java
@@ -5,12 +5,7 @@ import org.yaml.snakeyaml.Yaml;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Random;
-import java.util.Scanner;
-
-import pl.grzegorz2047.pobproject.Map;
+import java.util.*;
 
 /**
  * Created by s416045 on 2016-05-17.
@@ -20,14 +15,11 @@ public class Main {
 
     private static Random random = new Random();
 
-
-    private static Map map;
-
     public static void main(String[] args) {
-        map = new Map();
+
         BufferedReader br = null;
         try {
-            br = new BufferedReader(new FileReader("blocks.yml"));
+            br = new BufferedReader(new FileReader("mobs.yml"));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
@@ -36,14 +28,11 @@ public class Main {
         HashMap<String, ArrayList> config = (HashMap<String, ArrayList>) yaml.load(br);
 
         @SuppressWarnings("unchecked")
-        ArrayList<String> blocksStrings = (ArrayList<String>) config.get("blocks");
-        for (String entry : blocksStrings) {
-            Block b = BlocksManager.parseBlocks(entry);
-            map.getBlocks().put(b.toString(), b);
-        }
+        ArrayList<String> mobsStrings = (ArrayList<String>) config.get("mobs");
+
         Thread thread = new Thread(new Task());
         thread.start();
-        Scanner scanner = new Scanner(System.in);
+
        // while (scanner.)
     }
 
@@ -51,7 +40,6 @@ public class Main {
         return random;
     }
 
-    public static Map getMap() {
-        return map;
-    }
-}
+
+
+ }
--- src/main/java/pl/grzegorz2047/pobproject/Task.java
+++ src/main/java/pl/grzegorz2047/pobproject/Task.java
@@ -3,11 +3,9 @@ package pl.grzegorz2047.pobproject;
 /**
  * Created by s416045 on 2016-05-24.
  */
-public class Task implements Runnable {
+public class Task extends Thread {
 
     public void run() {
-        for (Mob mob : Main.getMap().getMobs()) {
-            mob.move();
-        }
+
     }
 }
